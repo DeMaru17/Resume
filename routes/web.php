08bb9fc1d5f22ab2b1f1c02f63f91ac4b6036682
Dennis Bhayu <dennisbayu99@gmail.com>
2024-08-23 05:42:33 +0700

    first commit, add login and dashboard feature, not fixed

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+// route login
+Route::get('login', [\App\Http\Controllers\LoginController::class, 'index']);
+Route::post('action-login',  [\App\Http\Controllers\LoginController::class, 'actionLogin'])->name('action-login');
+
+// route dashboard
+Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index']);
